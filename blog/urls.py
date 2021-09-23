c23f968ab7dff5cae824ecd37c92f465411492af
Jo <josephnkumah97@gmail.com>
2021-09-23 06:44:42 +0100

    Blog Class Based Views

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail,
-         name="post-detail-page"), #/posts/my-first-post
+    # path("", views.starting_page, name="starting-page"), # Function based view for Starting Page
+    path("", views.StartingPageView.as_view(), name="starting-page"), # Class based view for Starting Page
+    # path("posts/", views.posts, name="posts-page"), # Function based view for All Posts Page
+    path("posts/", views.AllPostsView.as_view(), name="posts-page"), # Class based view for All Posts Page
+    # path("posts/<slug:slug>", views.post_detail,
+    #      name="post-detail-page"), #/posts/my-first-post # Function based view for Single Post Page
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+         name="post-detail-page"), #/posts/my-first-post # Class based view for Single Post Page
 ]
